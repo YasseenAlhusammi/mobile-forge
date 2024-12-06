e4106424b1374693cd810bb4f34e48a76be3c5a0
YasseenAlhusammi <yasseen85@gmail.com>
2024-12-06 03:20:33 +0300

    poleas add

--- recipes/pyzbar/test.py
+++ recipes/pyzbar/test.py
@@ -5,7 +5,7 @@ import unittest
 class TestPyzbar(unittest.TestCase):
     def test_decode_barcode(self):
         # ضع مسار صورة تحتوي على باركود لاختبار المكتبة
-        image_path = 'path/to/barcode_image.png'  # استبدل بمسار الصورة الفعلي
+        image_path = 'code128.png'  # استبدل بمسار الصورة الفعلي
         image = Image.open(image_path)
         decoded_objects = pyzbar.decode(image)
         self.assertGreater(len(decoded_objects), 0, "No barcodes found")
